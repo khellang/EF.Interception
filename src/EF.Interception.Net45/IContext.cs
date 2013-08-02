ab03a236dbaa01ce9360c782eb8a1f74e00d65ea
Kristian Hellang <khellang@hotmail.com>
2013-08-02 15:21:57 +0200

    Last piece of documentation

--- src/EF.Interception.Net45/IContext.cs
+++ src/EF.Interception.Net45/IContext.cs
@@ -8,8 +8,16 @@ namespace EF.Interception
     /// <typeparam name="TEntity">The type of entities to intercept.</typeparam>
     public interface IContext<out TEntity> : IHideObjectMembers
     {
+        /// <summary>
+        /// Gets the entity that's being intercepted.
+        /// </summary>
+        /// <value>The entity.</value>
         TEntity Entity { get; }
 
+        /// <summary>
+        /// Gets or sets the entity's state.
+        /// </summary>
+        /// <value>The state.</value>
         EntityState State { get; set; }
     }
 }
