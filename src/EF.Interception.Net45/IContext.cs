9701a82b695cb983ef976a7058888b9a178e1c9f
Kristian Hellang <khellang@hotmail.com>
2013-08-02 17:47:12 +0200

    Added documentation to IValidationResult

--- src/EF.Interception.Net45/IContext.cs
+++ src/EF.Interception.Net45/IContext.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Data;
+﻿using System.Data;
 
 namespace EF.Interception
 {
--- src/EF.Interception.Net45/IValidationResult.cs
+++ src/EF.Interception.Net45/IValidationResult.cs
@@ -3,10 +3,21 @@ using System.Data.Entity.Validation;
 
 namespace EF.Interception
 {
+    /// <summary>
+    /// Validation result for an entity.
+    /// </summary>
     public interface IValidationResult : IHideObjectMembers
     {
+        /// <summary>
+        /// Gets the errors, if any.
+        /// </summary>
+        /// <value>The errors, if any.</value>
         IEnumerable<DbValidationError> Errors { get; }
 
+        /// <summary>
+        /// Gets a value indicating whether the entity is valid.
+        /// </summary>
+        /// <value><c>true</c> if the entity is valid; otherwise, <c>false</c>.</value>
         bool IsValid { get; }
     }
 }
