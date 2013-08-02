1647f9ccc3cf024bb5122b62daf2b088c821666d
Kristian Hellang <khellang@hotmail.com>
2013-08-02 22:27:04 +0200

    Added another test

--- test/EF.Interception.Tests/InterceptionDbContextTests.cs
+++ test/EF.Interception.Tests/InterceptionDbContextTests.cs
@@ -33,6 +33,30 @@ namespace EF.Interception.Tests
                 Assert.DoesNotThrow(() => _context.SaveChanges());
             }
 
+            [Fact]
+            public void ShouldNotDeleteEntitiesWhenStateIsSetToModifiedDuringInterception()
+            {
+                _context.AddInterceptor(new SoftDeleteInterceptor());
+
+                var book = new Book
+                {
+                    Name = "Harry Potter and the Prisoner of Azkaban",
+                    CreatedAt = DateTime.Now, // Need these for validation...
+                    ModifiedAt = DateTime.Now
+                };
+
+                _context.Books.Add(book);
+                _context.SaveChanges();
+
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+
+                var otherBook = _context.Books.Find(book.Id);
+
+                Assert.Same(book, otherBook);
+                Assert.True(book.IsDeleted);
+            }
+
             public void Dispose()
             {
                 _context.Dispose();
---
+++ test/EF.Interception.Tests/TestObjects/SoftDeleteInterceptor.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace EF.Interception.Tests
+{
+    public class SoftDeleteInterceptor : Interceptor<ISoftDeletedEntity>
+    {
+        public override void PreDelete(IContext<ISoftDeletedEntity> context)
+        {
+            context.Entity.IsDeleted = true;
+            context.State = EntityState.Modified;
+        }
+    }
+}
